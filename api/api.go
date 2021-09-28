aa418a769bbbf42aa8f9d766ebc33a034abff494
chuccp <cooge123@gmail.com>
2021-09-29 00:56:22 +0800

    Update api.go

--- api/api.go
+++ api/api.go
@@ -3,17 +3,31 @@ package api
 import (
 	"github.com/chuccp/cokePush/config"
 	"net/http"
+	"strconv"
 )
 
 type Server struct {
 	serveMux *http.ServeMux
+	config *config.Config
 
 }
+
+
 
 func (server *Server)root(w http.ResponseWriter, re *http.Request)  {
 
 }
-func (server *Server) Start(config *config.Config) {
+func (server *Server) Start(config *config.Config)error {
+	port:=config.GetIntOrDefault("rest.server.port",8080)
+	srv := &http.Server{
+		Addr: ":" + strconv.Itoa(port),
+		Handler:server.serveMux,
+	}
+	error:=srv.ListenAndServe()
+	return error
+}
+func (server *Server) Init() {
+
 
 }
 func (server *Server) AddRoute(pattern string, handler func(http.ResponseWriter, *http.Request)) {
