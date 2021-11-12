a981978d438f56b1a85f808fad6398f1db948915
chuccp <cooge123@gmail.com>
2021-11-12 14:51:10 +0800

    m

--- km/chunk.go
+++ km/chunk.go
@@ -6,115 +6,174 @@ import (
 )
 
 type chunk0 struct {
-	*Chunk
-	messageHeader []byte
+	*chunk
+	classType     byte
+	messageType   byte
+	messageLength uint32
+	messageId     uint32
+	time          uint32
 	key           byte
-	dataLen       int
+	dataLen       uint32
 	data          []byte
 }
 
-func createChunk0(classType byte, messageLength uint32, messageId uint32, time uint32, key byte, dataLen int, data []byte) *chunk0 {
-
-	return &chunk0{Chunk: &Chunk{classType}, messageHeader: messageHeader(messageLength, messageId, time), key: key, dataLen: dataLen, data: data}
+func createChunk0(chunkId byte, classType byte, messageType byte, messageLength uint32, messageId uint32, time uint32, key byte, dataLen uint32, data []byte) *chunk0 {
+	return &chunk0{chunk: &chunk{chunkId}, messageType: messageType, messageLength: messageLength, messageId: messageId, time: time, key: key, dataLen: dataLen, data: data}
 }
 
-func messageHeader(messageLength uint32, messageId uint32, time uint32) []byte {
+func (chunk0 chunk0) toByte() []byte {
+	bytesArray := make([]byte, 0)
+	//写chunk
+	bytesArray = append(bytesArray, chunk0.chunk.toByte()...)
+	//写header
+	bytesArray = append(bytesArray, chunk0.classType)
+	bytesArray = append(bytesArray, chunk0.messageType)
+	bytesArray = append(bytesArray, util.U32TOBytes(chunk0.time)...)
+	bytesArray = append(bytesArray, util.U32TOBytes(chunk0.messageId)...)
+	bytesArray = append(bytesArray, lengthToBytes(chunk0.messageLength)...)
+	//写body
+	bytesArray = append(bytesArray, chunk0.key)
+	bytesArray = append(bytesArray, lengthToBytes(chunk0.dataLen)...)
+	bytesArray = append(bytesArray, chunk0.data...)
+	return bytesArray
+}
 
-	if messageLength-32_767 < 0 {
+func lengthToBytes(length uint32) []byte {
+	if length-32_767 < 0 {
 		b := []byte{0, 0}
-		b[0] = byte(messageLength)
-		b[1] = byte(messageLength >> 8)
+		b[0] = byte(length)
+		b[1] = byte(length >> 8)
+		return b
 	} else {
 		b := []byte{0, 0, 0, 0}
-		var pre = (messageLength) | 32_768
+		var pre = (length) | 32_768
 		b[0] = byte(pre)
 		b[1] = byte(pre >> 8)
-		b[2] = byte(messageLength >> 15)
-		b[3] = byte(messageLength >> 23)
+		b[2] = byte(length >> 15)
+		b[3] = byte(length >> 23)
+		return b
 	}
-
-	util.U32TOBytes(messageId)
-
-	return nil
 }
 
 type chunk1 struct {
-	*Chunk
+	*chunk
 	key     byte
-	dataLen int
+	dataLen uint32
 	data    []byte
 }
 
-func createChunk1(classType byte, key byte, dataLen int, data []byte) *chunk1 {
+func createChunk1(classType byte, key byte, dataLen uint32, data []byte) *chunk1 {
 
-	return &chunk1{Chunk: &Chunk{64 | classType}}
+	return &chunk1{chunk: &chunk{64 | classType}}
+}
+
+func (chunk1 chunk1) toByte() []byte {
+	bytesArray := make([]byte, 0)
+	bytesArray = append(bytesArray, chunk1.chunk.toByte()...)
+	//写body
+	bytesArray = append(bytesArray, chunk1.key)
+	bytesArray = append(bytesArray, lengthToBytes(chunk1.dataLen)...)
+	bytesArray = append(bytesArray, chunk1.data...)
+
+	return bytesArray
 }
 
 type chunk2 struct {
-	*Chunk
+	*chunk
 	data []byte
 }
 
-func createChunk2(classType byte, data []byte) *chunk2 {
-	return &chunk2{Chunk: &Chunk{128 | classType}}
+func createChunk2(chunkId byte, data []byte) *chunk2 {
+	return &chunk2{chunk: &chunk{128 | chunkId}}
+}
+func (chunk2 chunk2) toByte() []byte {
+	bytesArray := make([]byte, 0)
+	bytesArray = append(bytesArray, chunk2.chunk.toByte()...)
+	bytesArray = append(bytesArray, chunk2.data...)
+	return bytesArray
 }
 
-type Chunk struct {
+type chunk struct {
 	chunkHeader byte
 }
 
-func (chunk *Chunk) setChunkHeader(chunkType byte, classType byte) {
-	chunk.chunkHeader = chunkType<<6 | classType
+func (chunk *chunk) setChunkHeader(chunkType byte, chunkId byte) {
+	chunk.chunkHeader = chunkType<<6 | chunkId
 }
-func (chunk *Chunk) chunkType() byte {
+func (chunk *chunk) chunkType() byte {
 	return chunk.chunkHeader >> 6
 }
-func (chunk *Chunk) classType() byte {
+func (chunk *chunk) chunkId() byte {
 	return chunk.chunkHeader << 2 >> 2
+}
+func (chunk *chunk) toByte() []byte {
+	return nil
 }
 
 type IChunk interface {
 	chunkType() byte
-	classType() byte
+	chunkId() byte
+	toByte() []byte
 }
 
 type chunkStream struct {
-	message  message.IMessage
-	process  int
-	dataLen  int
+	message message.IMessage
+	process int
+
+	messageLength  uint32
+	rMessageLength uint32
+
 	keyIndex byte
+	chunkId  byte
+
+	rdataLenTemp uint32
+	dataLenTemp  uint32
+	dataTemp     []byte
+	keyTemp      byte
 }
 
 func newChunkStream(message message.IMessage) *chunkStream {
-	return &chunkStream{message: message, process: 0, keyIndex: 0}
+	return &chunkStream{message: message, process: 0, keyIndex: 0, messageLength: message.GetMessageLength(), rMessageLength: 0}
 }
 func (stream *chunkStream) hasNext() bool {
 
-	return true
+	return stream.rMessageLength < stream.messageLength
 }
 func (stream *chunkStream) readChunk() IChunk {
-	key := stream.message.GetKeys()[stream.keyIndex]
-	data := stream.message.GetValue(key)
-	start := stream.dataLen
-	end := start + 512
-	if end > stream.dataLen-start {
-		end = stream.dataLen - start
-		stream.dataLen = end
-	}
-	if stream.process == 0 {
-		stream.process = 1
-		return createChunk0(stream.message.GetMessageType(), stream.message.GetMessageLength(), stream.message.GetMessageId(), stream.message.GetTimestamp(), key, len(data), data[start:end])
-	} else if stream.process == 1 {
 
-		chunk := createChunk1(stream.message.GetMessageType(), key, len(data), data[start:end])
-		if stream.dataLen > 0 {
+	if stream.process == 0 || stream.process == 1 {
+		stream.keyTemp = stream.message.GetKeys()[stream.keyIndex]
+		stream.dataTemp = stream.message.GetValue(stream.keyTemp)
+		stream.dataLenTemp = uint32(len(stream.dataTemp))
+		stream.rdataLenTemp = 0
+		stream.keyIndex++
+	}
+
+	start := stream.rdataLenTemp
+	end := start + 512
+	if end > stream.dataLenTemp-start {
+		end = stream.dataLenTemp
+	}
+	stream.rdataLenTemp = end
+	stream.rMessageLength = stream.rMessageLength + end - start
+	if stream.process == 0 {
+		chunk := createChunk0(stream.chunkId, stream.message.GetClassId(), stream.message.GetMessageType(), stream.message.GetMessageLength(), stream.message.GetMessageId(), stream.message.GetTimestamp(), stream.keyTemp, stream.dataLenTemp, stream.dataTemp[start:end])
+		if stream.rdataLenTemp < stream.dataLenTemp {
+			stream.process = 2
+		} else {
+			stream.process = 1
+		}
+		return chunk
+	} else if stream.process == 1 {
+		chunk := createChunk1(stream.message.GetMessageType(), stream.keyTemp, stream.dataLenTemp, stream.dataTemp[start:end])
+		if stream.rdataLenTemp < stream.dataLenTemp {
 			stream.process = 2
 		}
 		stream.keyIndex++
 		return chunk
 	} else if stream.process == 2 {
-		chunk := createChunk2(stream.message.GetMessageType(), data[start:end])
-		if stream.dataLen == 0 {
+		chunk := createChunk2(stream.message.GetMessageType(), stream.dataTemp[start:end])
+		if stream.rdataLenTemp == stream.dataLenTemp {
 			stream.process = 1
 		}
 		return chunk
--- message/message.go
+++ message/message.go
@@ -13,12 +13,24 @@ type IMessage interface {
 	GetClassId() byte
 	GetValue(key byte) []byte
 	GetKeys() []byte
-	SetValue(key byte, value interface{})
+	SetString(key byte, value string)
+	SetValue(key byte, value []byte)
 }
 
 type Message struct {
+	messageType   byte
+	classId       byte
+	keys          []byte
+	data          map[byte][]byte
+	messageLength uint32
+	time          uint32
+	messageId     uint32
 }
 
+func CreateMessage() *Message {
+
+	return &Message{messageId: msgId()}
+}
 func (message *Message) GetMessageId() uint32 {
 	return 0
 }
@@ -26,27 +38,32 @@ func (message *Message) GetTimestamp() uint32 {
 	return 0
 }
 func (message *Message) GetMessageLength() uint32 {
-	return 0
+	return message.messageLength
 }
-func (message *Message)GetClassId() byte{
-	return 0
+func (message *Message) GetClassId() byte {
+	return message.classId
 }
 func (message *Message) GetMessageType() byte {
-	return 0
+	return message.messageType
 }
 func (message *Message) GetValue(key byte) []byte {
-	return nil
+	return message.data[key]
 }
 func (message *Message) GetString(key byte) string {
 	return ""
 }
-func (message *Message) SetValue(key byte, value interface{}) {
-
+func (message *Message) SetString(key byte, value string) {
+	message.SetValue(key, []byte(value))
+}
+func (message *Message) SetValue(key byte, value []byte) {
+	message.keys = append(message.keys, key)
+	message.data[key] = value
+	message.messageLength = message.messageLength + uint32(len(value))
 }
 func (message *Message) GetKeys() []byte {
 	return nil
 }
-func GetMsgId() uint32 {
+func msgId() uint32 {
 	num := rand.Intn(1024)
 	return util.Millisecond()<<10 | (uint32(num))
 }
