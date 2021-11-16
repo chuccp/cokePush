327d67de28d3517c6822b9cb12373b1ceb70ef5f
chuccp <cooge123@gmail.com>
2021-11-16 14:01:37 +0800

    p

--- km/km.go
+++ km/km.go
@@ -2,45 +2,9 @@ package km
 
 import (
 	"github.com/chuccp/cokePush/message"
-	"github.com/chuccp/cokePush/net"
 	"io"
-	"sync"
 )
-var chunkWriteStreamPool *sync.Pool
-var chunkReadStreamPool *sync.Pool
-func init() {
-	chunkWriteStreamPool = &sync.Pool{New: func() interface{} {
-		return new(chunkWriteStream)
-	}}
-	chunkReadStreamPool = &sync.Pool{New: func() interface{} {
-		return new(chunkReadStream)
-	}}
-}
 
-func getChunkWriteStreamPool(msg message.IMessage)*chunkWriteStream  {
-	var chunkWriteStream = chunkWriteStreamPool.Get().(*chunkWriteStream)
-	chunkWriteStream.maxBodySize = 512
-	chunkWriteStream.chunkId = getChunkId()
-	chunkWriteStream.message = msg
-	chunkWriteStream.process = 0
-	chunkWriteStream.keyIndex = 0
-	chunkWriteStream.messageLength = msg.GetMessageLength()
-	chunkWriteStream.rMessageLength = 0
-	return chunkWriteStream
-}
-func getChunkReadStream(read_ io.Reader)* chunkReadStream {
-	var chunkReadStream = chunkReadStreamPool.Get().(*chunkReadStream)
-	chunkReadStream.read_ = net.NewIOReadStream(read_)
-	chunkReadStream.maxBodySize = 512
-	chunkReadStream.recordMap = make(map[uint16]*chunkRecord)
-	return chunkReadStream
-}
-func putChunkWriteStream(crm * chunkWriteStream )  {
-	chunkWriteStreamPool.Put(crm)
-}
-func putChunkReadStream(crm * chunkReadStream )  {
-	chunkReadStreamPool.Put(crm)
-}
 
 type km interface {
 	ReadMessage() (message.IMessage, error)
