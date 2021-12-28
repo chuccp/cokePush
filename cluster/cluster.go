751031da496c564d5ad72053ddf4928e74014fd9
chuccp <cooge123@gmail.com>
2021-12-28 23:25:55 +0800

    cluster

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -43,7 +43,7 @@ func (server *Server) AcceptConn() {
 		if err != nil {
 			break
 		} else {
-			client, err := NewClient(io,server)
+			client, err := NewClient(io, server)
 			if err == nil {
 				go client.Start()
 			}
@@ -51,103 +51,105 @@ func (server *Server) AcceptConn() {
 	}
 }
 
-func (server *Server)queryMachine(){
+func (server *Server) queryMachine() {
 	var hasQuery = false
 	for {
 		time.Sleep(time.Second * 2)
-		log.DebugF("查询机器信息 hasQuery:{} ",hasQuery)
-		if !hasQuery{
-			err:=server.queryMachineInfo()
-			log.DebugF("查询机器信息 hasQuery:{} ",hasQuery,err)
-			if err==nil{
+		log.DebugF("查询机器信息 hasQuery:{} ", hasQuery)
+		if !hasQuery && !server.machine.isLocal {
+			err := server.queryMachineInfo()
+			log.DebugF("查询机器信息 hasQuery:{} ", hasQuery, err)
+			if err == nil {
 				hasQuery = true
-			}else{
+			} else {
 				log.ErrorF("queryMachineInfo err:{}", err.Error())
 			}
 		}
-		if hasQuery{
+		if hasQuery {
 			server.getMachineList()
 		}
 		time.Sleep(time.Minute)
 	}
 }
+
 /**客户端获取请求机器信息**/
-func (server *Server) queryMachineInfo()error{
-	if !server.machine.isLocal{
-		qInfo:=newQueryMachineInfo(server.port, server.machineId)
-		log.DebugF("queryMachineInfo发送信息 :{} msgId:{}", server.machineId,qInfo.GetMessageId())
-		msg, conn,err := server.request.Call(server.machine.remoteHost,server.machine.remotePort,qInfo)
-		log.DebugF("queryMachineInfo 收到信息 :{}", server.machineId)
-		if err==nil{
-			if message.BackMessageClass == msg.GetClassId(){
-				if message.BackMessageOKType == msg.GetMessageType(){
-					machine :=msg.GetString(message.BackMachineAddress)
-					if len(machine)>0{
-						m,err:=toMachine(machine)
-						if err==nil{
-							if m.machineId==server.machineId{
-								m.isLocal = true
-								server.machine.isLocal = true
-								log.InfoF("连接到自己关闭连接 :{}",machine)
-								conn.Close()
-							}else{
-								m.remoteHost = server.machine.remoteHost
-								m.remotePort = server.machine.remotePort
-								if server.machineMap.add(m){
-									log.InfoF("queryMachineInfo 添加新的机器连接",machine)
-								}
-								return nil
+func (server *Server) queryMachineInfo() error {
+
+	qInfo := newQueryMachineInfo(server.port, server.machineId)
+	log.DebugF("queryMachineInfo发送信息 :{} msgId:{}", server.machineId, qInfo.GetMessageId())
+	msg, conn, err := server.request.Call(server.machine.remoteHost, server.machine.remotePort, qInfo)
+	log.DebugF("queryMachineInfo 收到信息 :{}", server.machineId)
+	if err == nil {
+		if message.BackMessageClass == msg.GetClassId() {
+			if message.BackMessageOKType == msg.GetMessageType() {
+				machine := msg.GetString(message.BackMachineAddress)
+				if len(machine) > 0 {
+					m, err := toMachine(machine)
+					if err == nil {
+						if m.machineId == server.machineId {
+							m.isLocal = true
+							server.machine.isLocal = true
+							log.InfoF("连接到自己关闭连接 :{}", machine)
+							conn.Close()
+							return nil
+						} else {
+							m.remoteHost = server.machine.remoteHost
+							m.remotePort = server.machine.remotePort
+							if server.machineMap.add(m) {
+								log.InfoF("queryMachineInfo 添加新的机器连接", machine)
 							}
-						}else{
-							return err
+							return nil
 						}
+					} else {
+						return err
 					}
 				}
 			}
-		}else{
-			return err
 		}
+	} else {
+		return err
 	}
 	return core.UnKnownConn
 }
+
 /**客户端获取机器列表**/
 func (server *Server) getMachineList() {
 	qMsg := newQueryMachineMessage(server.port, server.machineId)
 	server.machineMap.eachAddress(func(remoteHost string, remotePort int) {
-			msg,_,err := server.request.Call(remoteHost, remotePort, qMsg)
-			if err != nil {
-				log.ErrorF("getMachineList err:{}", err.Error())
-			} else {
-				if message.BackMessageClass == msg.GetClassId() {
-						if message.BackMessageOKType == msg.GetMessageType(){
-							machines:=msg.GetString(message.BackMachineAddress)
-							if len(machines)>0{
-								addresses:=strings.Split(machines,";")
-								if len(addresses)>0{
-									for _,v:=range addresses{
-										m,err:=toMachine(v)
-										if err!=nil{
-											log.ErrorF("getMachineList 解析地址错误 GetMessageType:{}  err:{}",v,err.Error())
-										}else{
-											if m.machineId==server.machineId{
-												m.isLocal = true
-											}else{
-												if server.machineMap.add(m){
-													log.InfoF("getMachineList 添加新的机器连接",v)
-												}
-											}
+		msg, _, err := server.request.Call(remoteHost, remotePort, qMsg)
+		if err != nil {
+			log.ErrorF("getMachineList err:{}", err.Error())
+		} else {
+			if message.BackMessageClass == msg.GetClassId() {
+				if message.BackMessageOKType == msg.GetMessageType() {
+					machines := msg.GetString(message.BackMachineAddress)
+					if len(machines) > 0 {
+						addresses := strings.Split(machines, ";")
+						if len(addresses) > 0 {
+							for _, v := range addresses {
+								m, err := toMachine(v)
+								if err != nil {
+									log.ErrorF("getMachineList 解析地址错误 GetMessageType:{}  err:{}", v, err.Error())
+								} else {
+									if m.machineId == server.machineId {
+										m.isLocal = true
+									} else {
+										if server.machineMap.add(m) {
+											log.InfoF("getMachineList 添加新的机器连接", v)
 										}
 									}
 								}
 							}
-						}else{
-							log.ErrorF("getMachineList 未知错误 GetMessageType：{}",msg.GetMessageType())
 						}
-				}else{
-					log.ErrorF("getMachineList 未知返回 BackMessageClass：{}",msg.GetClassId())
+					}
+				} else {
+					log.ErrorF("getMachineList 未知错误 GetMessageType：{}", msg.GetMessageType())
 				}
+			} else {
+				log.ErrorF("getMachineList 未知返回 BackMessageClass：{}", msg.GetClassId())
 			}
-		})
+		}
+	})
 }
 
 // Init 初始化
