74021ca5fe8dc42c8e85d9fa55cc7a93bcac749a
chuccp <cooge123@gmail.com>
2022-01-14 11:18:38 +0800

    queue

--- core/queue_test.go
+++ core/queue_test.go
@@ -2,12 +2,8 @@ package core
 
 import (
 	log "github.com/chuccp/coke-log"
-	"os"
-	"os/signal"
-	"runtime"
 	"sync"
 	"sync/atomic"
-	"syscall"
 	"testing"
 	"time"
 )
@@ -25,45 +21,91 @@ type queue struct {
 	input  *element
 	output *element
 	ch     chan bool
-	isWait int32
+	isWait bool
 	num    int32
 	rLock  *sync.RWMutex
+	timer  *timer
+}
+
+type timer struct {
+	t     *time.Timer
+	isEnd chan bool
+}
+
+func newTimer() *timer {
+	return &timer{t: time.NewTimer(time.Second * 10), isEnd: make(chan bool)}
+}
+func (timer *timer) wait() bool {
+	select {
+	case <-timer.t.C:
+		{
+			return false
+		}
+	case fa := <-timer.isEnd:
+		{
+			return fa
+		}
+	}
+	return false
+}
+func (timer *timer) end() {
+	fa := timer.t.Stop()
+	if fa {
+		timer.isEnd <- true
+	}
+}
+func (timer *timer) reset(duration time.Duration) {
+	timer.t.Reset(duration)
+}
+
+var poolTimer = &sync.Pool{
+	New: func() interface{} {
+		return newTimer()
+	},
+}
+
+func getTimer(duration time.Duration) *timer {
+	ti := poolTimer.Get().(*timer)
+	ti.reset(duration)
+	return ti
+}
+func freeTimer(timer *timer) {
+	poolTimer.Put(timer)
 }
 
 func newQueue() *queue {
-	return &queue{ch: make(chan bool), isWait: 0, num: 0, rLock: new(sync.RWMutex)}
+	return &queue{ch: make(chan bool), isWait: false, num: 0, rLock: new(sync.RWMutex)}
 }
 func (queue *queue) Offer(value interface{}) (num int32) {
 	ele := newElement(value)
 	queue.rLock.Lock()
-	if atomic.CompareAndSwapInt32(&(queue.num), 0, 1) {
+	if queue.num == 0 {
 		queue.input = ele
 		queue.output = ele
-		num = 1
-		queue.rLock.Unlock()
 	} else {
 		queue.input.next = ele
 		queue.input = ele
-		num = atomic.AddInt32(&queue.num, 1)
-		queue.rLock.Unlock()
 	}
-	if atomic.CompareAndSwapInt32(&(queue.isWait), 1, 0) {
+	num = atomic.AddInt32(&queue.num, 1)
+	if queue.isWait == true {
+		queue.isWait = false
+		queue.rLock.Unlock()
 		queue.ch <- true
+	} else {
+		queue.rLock.Unlock()
 	}
 	return
 }
 func (queue *queue) Poll() (value interface{}, num int32) {
 	for {
-		v := atomic.LoadInt32(&queue.num)
-		if v > 0 {
-			queue.rLock.Lock()
-			if atomic.CompareAndSwapInt32(&(queue.num), 1, 0) {
+		queue.rLock.Lock()
+		if queue.num > 0 {
+			if queue.num == 1 {
 				var ele = queue.output
-				value = ele.value
-				queue.output = ele.next
-				ele.next = nil
+				queue.num--
+				num = queue.num
 				queue.rLock.Unlock()
-				return
+				return ele.value, num
 			} else {
 				queue.rLock.Unlock()
 				var ele = queue.output
@@ -71,67 +113,81 @@ func (queue *queue) Poll() (value interface{}, num int32) {
 				queue.output = ele.next
 				ele.next = nil
 				num = atomic.AddInt32(&queue.num, -1)
+				return
 			}
-			return
 		} else {
-			atomic.StoreInt32(&(queue.isWait), 1)
-			queue.wait()
+			queue.isWait = true
+			queue.rLock.Unlock()
+			<-queue.ch
 		}
 	}
 }
-func (queue *queue) wait() {
-	<-queue.ch
+
+func (queue *queue) Take(duration time.Duration) (value interface{}, num int32) {
+	for {
+		queue.rLock.Lock()
+		if queue.num > 0 {
+			if queue.num == 1 {
+				var ele = queue.output
+				queue.num--
+				num = queue.num
+				queue.rLock.Unlock()
+				return ele.value, num
+			} else {
+				queue.rLock.Unlock()
+				var ele = queue.output
+				value = ele.value
+				queue.output = ele.next
+				ele.next = nil
+				num = atomic.AddInt32(&queue.num, -1)
+				return
+			}
+		} else {
+			queue.isWait = true
+			queue.rLock.Unlock()
+			queue.timer = getTimer(duration)
+			go func() {
+				fa := queue.timer.wait()
+				if !fa {
+					queue.rLock.Lock()
+					if queue.isWait == true {
+						queue.isWait = false
+						queue.rLock.Unlock()
+						queue.ch <- false
+					}
+				}
+			}()
+			flag := <-queue.ch
+			queue.timer.end()
+			freeTimer(queue.timer)
+			if !flag {
+				return nil, 0
+			}
+		}
+	}
 }
 
 func TestCompare(t *testing.T) {
 	que := newQueue()
 	go func() {
-		for {
-			v, num := que.Poll()
-			runtime.Gosched()
-			log.Info(v, "____", num)
+		for i:=0;i<100;i++{
+			go func() {
+				que.Offer(1)
+			}()
+		}
+		time.Sleep(time.Second*5)
+		for i:=0;i<100;i++{
+			go func() {
+				que.Offer(1)
+			}()
 		}
 	}()
-	time.Sleep(time.Second)
-	for i := 0; i < 1000; i++ {
 
-		go func() {
-			num := que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-			num = que.Offer(3)
-			log.Info("__!!!__", num)
-
-		}()
-
+	for{
+		v,num:=que.Take(time.Second)
+		log.Info(v,"=========",num)
 	}
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGBUS)
-	<-sig
+
+
 }
