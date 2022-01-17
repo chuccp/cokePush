d9f306accf6c1ffdd0271a7a159ab0c45f879664
chuccp <cooge123@gmail.com>
2022-01-17 10:39:59 +0800

    Create queue_test.go

---
+++ util/queue_test.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	log "github.com/chuccp/coke-log"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func Test_queue(t *testing.T)  {
+
+	var q = NewQueue()
+
+	for i := 0; i < 5; i++ {
+		go func() {
+			for  {
+				j := i
+				v,_:=q.Poll()
+				log.Info(j,"=====",v)
+			}
+
+		}()
+		time.Sleep(time.Second)
+	}
+
+	time.Sleep(time.Second+10)
+
+	q.Offer(1)
+	q.Offer(2)
+	q.Offer(3)
+	q.Offer(4)
+	q.Offer(5)
+
+	q.Offer(1)
+	q.Offer(2)
+	q.Offer(3)
+	q.Offer(4)
+	q.Offer(5)
+
+	q.Offer(1)
+	q.Offer(2)
+	q.Offer(3)
+	q.Offer(4)
+	q.Offer(5)
+
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGBUS)
+	<-sig
+
+}
